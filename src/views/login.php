3f865fbef7c3df894aa38ddd45a2c85892400f30
Fabiano Mendes <fabianomendesoficial@gmail.com>
2021-08-30 22:05:01 -0300

    Finalizing the visual part of the login

--- src/views/login.php
+++ src/views/login.php
@@ -7,11 +7,18 @@
             <form action="#" method="post">
                 <div class="mb-3">
                     <label for="email" class="form-label">Email</label>
-                    <input type="email" class="form-control login-email" id="email" placeholder="Digite o seu email">
+                    <input type="email" class="form-control" id="email" placeholder="Digite o seu email">
                 </div>
                 <div class="mb-3">
                     <label for="password" class="form-label">Senha</label>
-                    <input type="password" class="form-control login-password" id="password" placeholder="Digite a sua senha">
+                    <input type="password" class="form-control" id="password" placeholder="Digite a sua senha">
+                </div>
+                <div class="forgotPassword">
+                    <a href="#">Esqueci a senha</a>
+                </div>
+                <div class="buttons">
+                    <button class="btn bg-primary btn-login">Login</button>
+                    <button class="btn bg-secondary">Criar conta</button>
                 </div>
             </form>
         </div>   
