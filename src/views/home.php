6255b6bba2a64fc3800d117c7059cb88b599512f
Fabiano Mendes <fabianomendesoficial@gmail.com>
2021-08-31 05:08:00 -0300

    home page completion

--- src/views/home.php
+++ src/views/home.php
@@ -1,5 +1,22 @@
 <main class="main">
     <div class="container-md">
-
+        <a href="#">
+            <section class="area area-abstract">
+                <p>Resumos</p>
+                <img src="assets/img/Booklover-rafiki.svg" alt="Book lover-rafiki">
+            </section>
+        </a>
+        <a href="#">
+            <section class="area area-activity">
+                <p>Atividade</p>
+                <img src="assets/img/undraw_mathematics_4otb.svg" alt="undraw_mathematics">
+            </section>
+        </a>
+        <a href="#">
+            <section class="area area-classes">
+                <p>Aulas</p>
+                <img src="assets/img/Onlinelearning-amico.svg" alt="learning-amico">
+            </section>
+        </a>
     </div>
 </main>
--- src/views/template/footer.php
+++ src/views/template/footer.php
@@ -1,5 +1,6 @@
 <footer class="footer">
     <div class="container-md">
+        <!-- <a href="https://storyset.com/education">Education illustrations by Storyset</a> -->
     </div>
 </footer>
 </body>
