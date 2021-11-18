125bca2d57f999de511f222033a3d66a0c5cde92
Fabiano Mendes <fabianomendesoficial@gmail.com>
2021-11-17 23:15:47 -0300

    update

--- src/controllers/manageContent.php
+++ src/controllers/manageContent.php
@@ -6,8 +6,10 @@ require_once MODEL_PATH."/Data.php";
 
 if(isset($_GET['update']) && !empty($_GET['update'])){
     
+    
 }elseif(isset($_GET['delete']) && !empty($_GET['delete'])){
-    loadTemplateView("deleteContent", "Grants: delete", ['home'], ['menu-toggle'], true);
+    $data = Data::getOne(['id' => base64_decode($_GET['delete'])]);
+    loadTemplateViewWithGetOne('deleteContent', "Grants: Delete", $data, ['deleteContent'], ['menu-toggle'], true);
 }else{
     header("Location: areas?a=re&mat=all");
 }
---
+++ src/views/deleteContent.php
@@ -0,0 +1,16 @@
+<main class="main">
+    <div class="container-md">
+        <section class="main-section">
+            <h1>Tem certeza que deseja deletar ?</h1>
+
+            <p>Título: <?= $data->title ?></p>
+            
+            <p>Subtítulo: <?= $data->abstract ?></p>
+
+            <div class="buttons">
+                <button class="btn btn-primary">Sim</button>
+                <button class="btn btn-secondary">Não</button>
+            </div>
+        </section>
+    </div>
+</main>
