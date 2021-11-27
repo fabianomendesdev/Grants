073999336dd491e8aef14769c72c8305009a5060
Fabiano Mendes <fabianomendesoficial@gmail.com>
2021-11-27 13:14:09 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,29 @@
 <?php 
-require_once(dirname(__FILE__, 2). '/src/config/config.php');
+require(dirname(__FILE__, 2). '/src/config/config.php');
 
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
-if($uri === '' || $uri === '/' || $uri === 'index.php') {
+$uri = str_replace(['.php', '.js', '.html'], '', $uri);
+
+if($uri == '' || $uri == '/' || $uri == 'index.php'){
     $uri = 'home';
+}
+
+$array_uri = str_split($uri);
+$tempUri = '';
+foreach($array_uri as $key => $letter){
+    if($key > 0){
+        if($letter != '/'){
+            $tempUri .= $letter;
+        }else{
+            $uri = "/".$tempUri;
+            if(array_key_last($array_uri) > $key){
+                $uri = $uri."?".parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
+                header("Location: $uri");
+            }
+            break;
+        }
+    }
 }
 
 require_once(CONTROLLER_PATH . "/$uri.php");
